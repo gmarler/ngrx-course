636102f27adadb25de1dd113a7376d6bfe23b551
gmarler <gmarler@gmarler.com>
2017-04-13 19:22:13 -0400

    Add @Effect decorator to load-threads-effect service

--- src/app/store/effects/load-threads-effect.service.ts
+++ src/app/store/effects/load-threads-effect.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
+import {ThreadsService} from '../../services/threads.service';
+import {Actions, Effect} from '@ngrx/effects';
+import {LOAD_USER_THREADS_ACTION, UserThreadsLoadedAction} from '../actions';
 
 @Injectable()
 export class LoadThreadsEffectService {
 
-  constructor() { }
+  constructor(private actions$: Actions,
+              private threadsService: ThreadsService) {
+
+  }
+
+  @Effect() userThreads$ = this.actions$
+    .ofType(LOAD_USER_THREADS_ACTION)
+    .switchMap(() => this.threadsService.loadUserThreads())
+    .map(allUserData => new UserThreadsLoadedAction(allUserData));
 
 }
