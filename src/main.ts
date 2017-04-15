a9b9441c974b72f7d4fb66afb898a00d6a597387
gmarler <gmarler@gmarler.com>
2017-04-15 13:46:50 -0400

    Add Observable.debug utility extension

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,37 @@ import { AppModule } from './app/';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/skip';
 import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/do';
+import {Observable} from "rxjs/Observable";
+
+const debuggerOn = true;
+
+Observable.prototype.debug = function(message: string) {
+  return this.do(
+    nextValue => {
+      if (debuggerOn) {
+        console.log(message, nextValue);
+      }
+    },
+    error => {
+      if (debuggerOn) {
+        console.error(message, error);
+      }
+    },
+    () => {
+      if (debuggerOn) {
+        console.log("Observable completed - ", message);
+      }
+    }
+  );
+};
+
+// Extend rxjs/Observable with debug property
+declare module 'rxjs/Observable' {
+  interface Observable<T> {
+    debug: (...any) => Observable<T>
+  }
+}
 
 if (environment.production) {
   enableProdMode();
