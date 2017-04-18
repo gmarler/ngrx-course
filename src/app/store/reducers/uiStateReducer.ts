0d7ad98a07c8b6bf5ba7d635a35c4722663e8d5a
gmarler <gmarler@gmarler.com>
2017-04-17 23:42:51 -0400

    reducer for SELECT_USER_ACTION

--- src/app/store/reducers/uiStateReducer.ts
+++ src/app/store/reducers/uiStateReducer.ts
@@ -1,26 +1,28 @@
-
-
-
 import {UiState, INITIAL_UI_STATE} from '../ui-state';
 import {Action} from '@ngrx/store';
-import {THREAD_SELECTED_ACTION} from '../actions';
+import {SELECT_USER_ACTION, SelectUserAction, THREAD_SELECTED_ACTION} from '../actions';
 
 
 export function uiState(state: UiState = INITIAL_UI_STATE, action: Action): UiState {
 
-    switch (action.type)  {
+  switch (action.type) {
 
-        case THREAD_SELECTED_ACTION:
+    case THREAD_SELECTED_ACTION:
+      const newState = Object.assign({}, state);
+      newState.currentThreadId = action.payload;
+      return newState;
 
-            const newState = Object.assign({}, state);
+    case SELECT_USER_ACTION:
+      return handleSelectUserAction(state, action);
 
-            newState.currentThreadId = action.payload;
+    default:
+      return state;
+  }
 
-            return newState;
+}
 
-
-        default:
-            return state;
-    }
-
+function handleSelectUserAction(state: UiState, action: SelectUserAction) {
+  const newUiState = Object.assign({}, state);
+  newUiState.userId = action.payload;
+  return newUiState;
 }
--- src/app/store/ui-state.ts
+++ src/app/store/ui-state.ts
@@ -1,12 +1,10 @@
-
-
 export interface UiState {
-    userId:number;
-    currentThreadId: number;
+  userId:          number;
+  currentThreadId: number;
 }
 
 
 export const INITIAL_UI_STATE: UiState = {
-    userId: 1,
-    currentThreadId: undefined
+  userId:          undefined,
+  currentThreadId: undefined
 };
