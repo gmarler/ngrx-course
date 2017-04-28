f3885e30036ccb3a8375be1581cccd48bb56aa59
gmarler <gmarler@gmarler.com>
2017-04-27 21:48:06 -0400

    Corrected store.select('string') with store.select<Type>('string')
    From https://github.com/ngrx/store/issues/292

--- src/app/store/effects/server-notifications-effect.service.ts
+++ src/app/store/effects/server-notifications-effect.service.ts
@@ -6,6 +6,7 @@ import {NewMessagesReceivedAction} from '../actions';
 import {ApplicationState} from '../application-state';
 import {Store} from '@ngrx/store';
 import {uiState} from '../reducers/uiStateReducer';
+import {UiState} from "../ui-state";
 
 @Injectable()
 export class ServerNotificationsEffectService {
@@ -13,12 +14,12 @@ export class ServerNotificationsEffectService {
               private store: Store<ApplicationState>) { }
 
   @Effect() newMessages$ = Observable.interval(3000)
-    .withLatestFrom(this.store.select('uiState'))
+    .withLatestFrom(this.store.select<UiState>('uiState'))
     .map(([any, uiState]) => uiState)
     .filter(uiState => uiState.userId)
     .switchMap(uiState => this.threadsService.loadNewMessagesForUser(uiState.userId))
     .debug("new messages received from server")
-    .withLatestFrom(this.store.select('uiState'))
+    .withLatestFrom(this.store.select<UiState>('uiState'))
     .map(([unreadMessages, uiState]) =>
       new NewMessagesReceivedAction({
         unreadMessages,
