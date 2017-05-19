7ed9699055a1eb17bdd55172065b13c67ec9d971
gmarler <gmarler@gmarler.com>
2017-05-19 00:09:23 -0400

    Start work on OnPush change detections

--- src/app/thread-list/thread-list.component.ts
+++ src/app/thread-list/thread-list.component.ts
@@ -1,33 +1,34 @@
-import {Component, OnInit, Input, EventEmitter, Output} from '@angular/core';
+import {Component, OnInit, Input, EventEmitter, Output, ChangeDetectionStrategy} from '@angular/core';
 import {ThreadSummaryVM} from "../thread-section/thread-summary.vm";
 
 @Component({
-    selector: 'thread-list',
-    templateUrl: './thread-list.component.html',
-    styleUrls: ['./thread-list.component.css']
+  selector: 'thread-list',
+  templateUrl: './thread-list.component.html',
+  styleUrls: ['./thread-list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ThreadListComponent implements OnInit {
 
-    @Input()
-    threads: ThreadSummaryVM[];
+  @Input()
+  threads: ThreadSummaryVM[];
 
-    @Input()
-    currentSelectedThreadId:number;
-
-
-    @Output()
-    threadSelected = new EventEmitter();
+  @Input()
+  currentSelectedThreadId: number;
 
 
-    constructor() {
-    }
+  @Output()
+  threadSelected = new EventEmitter();
 
-    ngOnInit() {
 
-    }
+  constructor() {
+  }
 
-    selectThread(threadId:number) {
-        this.threadSelected.next(threadId);
-    }
+  ngOnInit() {
+
+  }
+
+  selectThread(threadId: number) {
+    this.threadSelected.next(threadId);
+  }
 
 }
